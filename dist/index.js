84ed3393227719437c35a6bc022e7076d0aeef51
= <=>
2023-04-30 11:21:53 -0300

    remove console message

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,6 @@ class ObjectValidator {
         const result = getRules.execute(dummy, rule);
         const ruleList = result[0];
         const dummyList = result[1];
-        console.log(dummyList, ruleList);
         if (JSON.stringify(ruleList) === JSON.stringify(dummyList))
             return true;
         return false;
